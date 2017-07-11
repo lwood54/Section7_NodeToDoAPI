804ab2b034ff6938b3d706af5bc2285bf9cf3ef9
Logan Wood <lwood3499@gmail.com>
2017-07-11 18:17:13 -0500

    added tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,8 +5,23 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+// The problem with the way it was, is that the database is wiped clean before each
+// test. When we test GET /todos, we want there to be some data that we can test
+// to make sure all is going well with GET as well as POST.
+// we will make up an array of dummy 'todos' to include some 'seed' data, that way our database remains
+// stable and predictable
+// SEED DATA
+const todos = [{
+    text: 'Firest test todo'
+}, {
+    text: 'Second test todo'
+    }
+];
+// now we have to use a mongoose method called 'insertMany()'
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -24,8 +39,8 @@ describe('POST /todos', () => {
             if (err) {
                 return done(err);
             }
-            Todo.find().then((todos) => {
-                expect(todos.length).toBe(1);
+            Todo.find({text: text}).then((todos) => { // NOTE: I changed it from {text} to {text: text} for clarity
+                expect(todos.length).toBe(1);           // but ES6 lets us do {text} when the key and value are the same
                 expect(todos[0].text).toBe(text);
                 done();
             }).catch((error) => done(e));
@@ -42,9 +57,21 @@ describe('POST /todos', () => {
                     return done(err);
                 }
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((error) => done(error));
             });
+    });
+
+    describe('GET /todos', () => {
+        it('should get all todos', (done) => {
+            request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+        });
     });
 });
