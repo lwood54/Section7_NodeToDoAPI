2ba8ef2095c254298c55680092fd88177b38f3de
Logan Wood <lwood3499@gmail.com>
2017-07-14 16:24:33 -0500

    hashed user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,44 +1,17 @@
-// get access to hashing function
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
 
-// setting up and assigning a token with a secret. sign(objectToPass, 'secret')
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (error, salt) => {
+//     bcrypt.hash(password, salt, (error, hash) => {
+//         console.log(hash);
+//     });
+// });
 
-// verifying token
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$RczOa97lD9SlhoUCOsV4cuTmJCUzSZkYqkB/oHKJaqNZEb23lRSwG';
 
-// var message = 'I am user number 54';
-// var hash = SHA256(message).toString(); // before string conversion, SHA256() returns an object
-//
-// console.log(`Message: ${message}`); // Message: I am user number 54
-// console.log(`Hash: ${hash}`);   // 410a0234fd56075ece21c295ff8c88735b37e7adc6ab44e16ccd26eff31a6bd4
-//
-// var data = {
-//     id: 4
-// };
-// var token = {
-//     data,
-//     hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-// };
-// // salting the hash means adding on some secret addition to the end of the hash so some other
-// // user couldn't just use the same hash and get the same result
-//
-// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
-//
-// token.data.id = 5;
-// token.hash = SHA256(JSON.stringify(token.data)).toString();
-// // This might be what a middle player would do in order to manipulate data.
-// // Their problem is that they don't have the 'somesecret' salt added. So they aren't able to have the sam hash output.
-//
-// if (resultHash === token.hash) {
-//     console.log('Data was not changed');
-// } else {
-//     console.log("Data was changed. Don't trust.");
-// }
+bcrypt.compare(password, hashedPassword, (error, result) => {
+    console.log(result);
+});
