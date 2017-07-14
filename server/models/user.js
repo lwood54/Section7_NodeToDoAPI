8a884ebdc18364280dffe1841691e8212460e0f1
Logan Wood <lwood3499@gmail.com>
2017-07-14 18:44:19 -0500

    added POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -74,6 +74,25 @@ UserSchema.statics.findByToken = function(token) {
     });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+    var User = this;
+
+    return User.findOne({email}).then(function(user) {
+        if (!user) {
+            return Promise.reject();
+        }
+        return new Promise(function(resolve, reject) {
+            bcrypt.compare(password, user.password, function(error, response) {
+                if (response) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+        });
+    });
+};
+
 UserSchema.pre('save', function(next) {
     var user = this;
 
