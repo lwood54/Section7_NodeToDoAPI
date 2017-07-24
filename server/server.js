ff5f12cb338641c66e5e67fe82a4f4ef4f9ba64d
Logan Wood <lwood3499@gmail.com>
2017-07-24 17:05:51 -0500

    changed some functions to async/await

--- server/server.js
+++ server/server.js
@@ -63,26 +63,50 @@ app.get('/todos/:id', authenticate, (req, res) => {
     });
 });
 
-app.delete('/todos/:id', authenticate, (req, res) => {
-    var id = req.params.id;
-    if (!ObjectID.isValid(id)) {
-        console.log("non valid id response sent");
-        return res.status(404).send();
-    }
-    Todo.findOneAndRemove({
-        _id: id,
-        _creator: req.user._id
-    }).then((todo) => {
+//// ASYN/AWAIT VERSION for app.delete('/todos/:id') ////
+app.delete('/todos/:id', authenticate, async (req, res) => {
+    try {
+        var id = req.params.id;
+        if (!ObjectID.isValid(id)) {
+            console.log("non valid id response sent");
+            return res.status(404).send();
+        }
+        const todo = await Todo.findOneAndRemove({
+            _id: id,
+            _creator: req.user._id
+        });
         if (!todo) {
             console.log("valid id, but not in db");
             return res.status(404).send();
         }
         console.log("removed by id successful");
         return res.send({todo});
-    }).catch((error) => {
+    } catch (err) {
         return res.status(400).send();
-    });
+    }
 });
+
+/// ORIGINAL VERSION ////
+// app.delete('/todos/:id', authenticate, (req, res) => {
+//     var id = req.params.id;
+//     if (!ObjectID.isValid(id)) {
+//         console.log("non valid id response sent");
+//         return res.status(404).send();
+//     }
+//     Todo.findOneAndRemove({
+//         _id: id,
+//         _creator: req.user._id
+//     }).then((todo) => {
+//         if (!todo) {
+//             console.log("valid id, but not in db");
+//             return res.status(404).send();
+//         }
+//         console.log("removed by id successful");
+//         return res.send({todo});
+//     }).catch((error) => {
+//         return res.status(400).send();
+//     });
+// });
 
 app.patch('/todos/:id', authenticate, (req, res) => {
     var id = req.params.id;
@@ -116,49 +140,89 @@ app.patch('/todos/:id', authenticate, (req, res) => {
 
 ////////////////////////////// USERS //////////////////////
 
-// POST /users
-app.post('/users', (req, res) => {
-    var body = _.pick(req.body, ['email', 'password']);
-    var user = new User({   // we could have just put var user = new User(body) since we already made an object with JUST email and password
-        email: body.email,
-        password: body.password
-    });
-    user.save().then(() => {
-        return user.generateAuthToken();
-        // res.send(user);
-    }).then((token) => {
+/// ASYN/AWAIT VERSION of app.post('/users')
+app.post('/users', async (req, res) => {
+    try {
+        var body = _.pick(req.body, ['email', 'password']);
+        var user = new User(body);
+        await user.save();
+        const token = await user.generateAuthToken();
         res.header('x-auth', token).send(user);
-    }).catch((error) => {
+    } catch(error) {
         res.status(400).send(error);
-    });
+    }
 });
+/// ORIGINAL ///
+// POST /users
+// app.post('/users', (req, res) => {
+//     var body = _.pick(req.body, ['email', 'password']);
+//     var user = new User({   // we could have just put var user = new User(body) since we already made an object with JUST email and password
+//         email: body.email,
+//         password: body.password
+//     });
+//     user.save().then(() => {
+//         return user.generateAuthToken();
+//         // res.send(user);
+//     }).then((token) => {
+//         res.header('x-auth', token).send(user);
+//     }).catch((error) => {
+//         res.status(400).send(error);
+//     });
+// });
 
 // GET /users/me    || creating a private route
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
+
+//// ASYNC/AWAIT VERSION of app.post('/users/login', function(req, res) ////
+app.post('/users/login', async function(req, res) {
+    try {
+        const body = _.pick(req.body, ['email', 'password']);
+        const user = await User.findByCredentials(body.email, body.password);
+        const token = await user.generateAuthToken();
+        res.header('x-auth', token).send(user);
+    } catch (err) {
+        res.status(400).send();
+    }
+});
+
 // POST /users/login {email, password}
 // to verify that the user was located, use res.send() and send back the body data
-app.post('/users/login', function(req, res) {
-    var body = _.pick(req.body, ['email', 'password']);
+//// ORIGINAL VERSION ////
+// app.post('/users/login', function(req, res) {
+//     var body = _.pick(req.body, ['email', 'password']);
+//
+//     User.findByCredentials(body.email, body.password).then(function(user) {
+//         return user.generateAuthToken().then(function(token) {
+//             res.header('x-auth', token).send(user);
+//         });
+//     }).catch(function(error) {
+//         res.status(400).send();
+//     });
+// });
 
-    User.findByCredentials(body.email, body.password).then(function(user) {
-        return user.generateAuthToken().then(function(token) {
-            res.header('x-auth', token).send(user);
-        });
-    }).catch(function(error) {
-        res.status(400).send();
-    });
-});
 
-app.delete('/users/me/token', authenticate, function(req, res) {
-    req.user.removeToken(req.token).then(function() {
+
+//// ASYNC/AWAIT VERSION of app.delete('/users/me/token') ////
+app.delete('/users/me/token', authenticate, async function(req, res) {
+    try {
+        await req.user.removeToken(req.token);
         res.status(200).send();
-    }, function() {
+    } catch (err) {
         res.status(400).send();
-    });
+    }
 });
+
+//// ORIGINAL VERSION ////
+// app.delete('/users/me/token', authenticate, function(req, res) {
+//     req.user.removeToken(req.token).then(function() {
+//         res.status(200).send();
+//     }, function() {
+//         res.status(400).send();
+//     });
+// });
 
 // GET /todos/dynamicIDintake
 app.get('/users/:id', (req, res) => {
